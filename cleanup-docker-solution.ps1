e55382182430b80790cf8f0aecb96aba9689e208
afroze9 <afrozeamjad@gmail.com>
2023-07-02 20:08:52 +0500

    Removing most powershell scripts

--- cleanup-docker-solution.ps1
+++
@@ -1,3 +0,0 @@
-docker-compose -f .\docker-compose.yml down -v
-docker-compose -f .\discovery-server\docker\docker-compose.yml down -v
-
--- cleanup-local-solution.ps1
+++
@@ -1,3 +0,0 @@
-docker-compose -f .\docker-compose-local.yml down -v
-docker-compose -f .\discovery-server\docker\docker-compose.yml down -v
-
