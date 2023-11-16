51a8260264ffbdb8e6549f0e3205404089e0f3ce
afroze9 <afrozeamjad@gmail.com>
2023-11-16 11:04:24 +0500

    Adding GRPC to Project API

--- services/project-api/src/Nexus.Project.Api/Mapping/ProjectProfile.cs
+++ services/project-api/src/Nexus.Project.Api/Mapping/ProjectProfile.cs
@@ -2,6 +2,7 @@
 using Nexus.ProjectAPI.Entities;
 using Nexus.ProjectAPI.Models;
 using Nexus.SharedKernel.Contracts.Project;
+using ProjectEntity = Nexus.ProjectAPI.Entities.Project;
 
 namespace Nexus.ProjectAPI.Mapping;
 
@@ -10,13 +11,13 @@ public class ProjectProfile : Profile
 {
     public ProjectProfile()
     {
-        CreateMap<Project, ProjectRequestModel>();
-        CreateMap<ProjectRequestModel, Project>();
+        CreateMap<ProjectEntity, ProjectRequestModel>();
+        CreateMap<ProjectRequestModel, ProjectEntity>();
         CreateMap<TodoItemRequestModel, TodoItem>();
 
-        CreateMap<Project, ProjectResponseModel>()
+        CreateMap<ProjectEntity, ProjectResponseModel>()
             .ForMember(dest => dest.Priority, opt => opt.MapFrom(src => src.Priority));
-        CreateMap<ProjectResponseModel, Project>();
+        CreateMap<ProjectResponseModel, ProjectEntity>();
         CreateMap<TodoItem, TodoItemResponseModel>();
     }
 }
---
+++ services/project-api/src/Nexus.Project.Api/Services/ProjectGrpcService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using Nexus.ProjectAPI.Data;
+using Nexus.SharedKernel.GRPC;
+using ProjectEntity = Nexus.ProjectAPI.Entities.Project;
+
+namespace Nexus.ProjectAPI.Services;
+
+public class ProjectGrpcService : SharedKernel.GRPC.ProjectGrpcService.ProjectGrpcServiceBase
+{
+    private readonly UnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public ProjectGrpcService(UnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public override async Task<GetProjectsByCompanyIdResponse> GetProjectsByCompanyId(
+        GetProjectsByCompanyIdRequest request, 
+        ServerCallContext context)
+    {
+        List<ProjectEntity> projects = await _unitOfWork.Projects.GetAllByCompanyIdAsync(request.CompanyId, true);
+        List<Project> mappedProjects =
+            projects.Count == 0 ? new List<Project>() : _mapper.Map<List<Project>>(projects);
+
+        GetProjectsByCompanyIdResponse response = new ()
+        {
+            Projects = { mappedProjects },
+        };
+
+        return response;
+    }
+}
